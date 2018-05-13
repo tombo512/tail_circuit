5dfd453d025220002dfe79f31b85df833f4a2ff6
riekohirota <crista.p.p.j@gmail.com>
2018-05-13 19:51:38 +0900

    changed servo module

--- Tail_Circuit.cpp
+++ Tail_Circuit.cpp
@@ -7,97 +7,87 @@
   2018/5/5
   R.Hirota
 
-  Arduino Nano
-
-  < Pin assign >
-  D9 to rudder servo
-  D10 to elevator servo
-  D2 to switch right
-  D3 to switch left
-  A0 to slide volume
+  assign pin D9 to rudder PWM
+  assign pin D10 to elevator PWM
+  assign pin D2 to switch right
+  assign pin D3 to switch left
+  assign pin A0 to slide volume
 *******************
 
 */
 
-#include <VarSpeedServo.h>
-VarSpeedServo 
+#include <Servo.h>
+Servo 
   rudder, 
   elevator;
 
 // Pin assighn
-int Rudder_Output_Pin = 9,
-    Elevator_Output_Pin = 10,
-    Switch_Input_right_Pin = 2,
-    Switch_Input_left_Pin = 3,
-    Analog_Input_Pin = 0;
+int Rudder_Output_Pin = 9;
+int Elevator_Output_Pin = 10;
+int Switch_Input_right_Pin = 2;
+int Switch_Input_left_Pin = 3;
+int Analog_Input_Pin = 0;
 
 // Values
-bool val_right = 0,
-      val_left = 0;
-int val_elv = 0,
-    get_analog = 0,
-    n_steps = 5,
-    bit_shift = 3;
+bool val_right = 0;
+bool val_left = 0;
+int val_elv = 0;
+int get_analog = 0;
+int n_steps = 5;
+int bit_shift = 3;
 
 // Paramaters
-int MAX_RDR_ANDLE = 20,
-    MAX_ELV_ANGLE = 30,
-    CENTER_RDR_ANGLE = 90,
-    CENTER_ELV_ANGLE = 90,
-    SERVO_MIN_PULSE = 750,
-    SERVO_MAX_PULSE = 2250,
-    SERVO_ANGULAR_VEL = 150;
+int rudder_angle = 90;
+int elevator_angle = 10;
 
-int rudder_angle = CENTER_RDR_ANGLE,
-    elevator_angle = CENTER_ELV_ANGLE;
+int MAX_RDR_ANDLE = 20;
+int MAX_ELV_ANGLE = 30;
 
 void setup() {
   Serial.begin(9600);
-
-  rudder.attach(Rudder_Output_Pin, SERVO_MIN_PULSE,
-                SERVO_MAX_PULSE);
-  elevator.attach(Elevator_Output_Pin, SERVO_MIN_PULSE, 
-                  SERVO_MAX_PULSE);
-
+  rudder.attach(Rudder_Output_Pin, 750, 2250);
+  elevator.attach(Elevator_Output_Pin, 750, 2250);
   pinMode(Switch_Input_right_Pin, INPUT);
   pinMode(Switch_Input_left_Pin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
-int and_check(bool val_right, bool val_left){
+int check(bool val_right, bool val_left){
   bool check_mlt = val_right*val_left;
   return check_mlt;
 }
 
 void loop() {
   while(1){
+    /*
+    digitalWrite(4, HIGH);
+    delay(500);
+    digitalWrite(4, LOW);
+    delay(500);
+    */
     val_right = digitalRead(Switch_Input_right_Pin);
     val_left = digitalRead(Switch_Input_left_Pin);
-
-    if(!and_check(val_right, val_left)){  // XOR
+    if(!check(val_right, val_left)){
       rudder_angle = 
-        CENTER_RDR_ANGLE - MAX_RDR_ANDLE*val_left + MAX_RDR_ANDLE*val_right;
-      rudder.write(rudder_angle, SERVO_ANGULAR_VEL, false);
-      delay(10);
+        90 - MAX_RDR_ANDLE*val_left + MAX_RDR_ANDLE*val_right;
+      rudder.write(rudder_angle);
+      delay(50);
     }else{
-      rudder.write(rudder_angle, SERVO_ANGULAR_VELE, false);
+      rudder.write(rudder_angle); // keep
     }
 
-    val_elv = 0;  // reset
+   val_elv = 0;
 
-    for(int i = 0; i < n_steps; i++){
-      get_analog = analogRead(Analog_Input_Pin);
-      get_analog >>= bit_shift;
-      val_elv += get_analog;
-      delay(10);
-    }
-
-    val_elv = val_elv / n_steps;  // avarage
-    val_elv = val_elv - ((1024 >>= bit_shift)*0.5);
-    val_elv = val_elv / ((1024 >>= bit_shift)*0.5); // normarization
-
-    Serial.println(val_elv, DEC);
-    elevator_angle = CENTER_ELV_ANGLE + val_elv*MAX_ELV_ANGLE;
-    elevator.write(elevator_angle, SERVO_ANGULAR_VEL, false);
+   for(int i = 0; i < 5; i++){
+     get_analog = analogRead(Analog_Input_Pin);
+     get_analog >>= bit_shift;
+     val_elv += get_analog;
+     delay(10);
+   }
+   val_elv = val_elv / 5;
+   Serial.println(val_elv, DEC);
+   elevator_angle = 10 + val_elv;
+   elevator.write(elevator_angle);
   }
 }
